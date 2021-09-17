9325210d724454657ce22d01f862a0f72bf64478
João Victor <joaoeusoueu13225@gmail.com>
2021-09-17 15:24:56 -0300

    finalização

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert("eae")
+function imagens(){
+
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("imagem")
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `agora são ${hora} horas.`
+    
+    if (hora >= 5 && hora < 12) {
+
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#efc897'
+
+    } else if (hora > 12 && hora <= 18) {
+
+        img.src = 'tarde.png'
+        document.body.style.background = '#f9ae54'
+
+    } else {
+
+        img.src = 'noite.png'
+        document.body.style.background = '#294a54'
+
+    }
+
+}
