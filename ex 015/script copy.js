b64c36eadedccd56d31a59ba35ac90b4c7721860
João Victor <joaoeusoueu13225@gmail.com>
2021-09-28 18:56:06 -0300

    atualizaççao

--- ex 015/script copy.js
+++ ex 015/script copy.js
@@ -2,14 +2,50 @@ function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
+    var res = document.querySelector('div#res')
     if(fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
         
     } else {
-        var fsex = document.getElementsByName('checksex')
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada : ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+
+            genero = 'Homem'
+
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'nene.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovemhomi.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'homi.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'vei.png')
+            }
+        } else if (fsex[1].checked) {
+
+            genero = 'Mulher'
+
+            if (idade >= 0 && idade < 10) {
+                //criança
+            } else if (idade < 21) {
+                //jovem
+            } else if (idade < 50) {
+                //adulta
+            } else {
+                //idosa
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
         
     }
 
