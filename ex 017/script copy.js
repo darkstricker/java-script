54228fefa9843214e4bf1b2fe34c668995aff0fd
João Victor <joaoeusoueu13225@gmail.com>
2021-10-19 17:51:50 -0300

    atualização

--- ex 017/script copy.js
+++ ex 017/script copy.js
@@ -5,11 +5,16 @@ function tabuada() {
         window.alert ('por favor digite um número!')
     } else {
         let n = Number(num.value)
-        let c = 1}
+        let c = 1
+        tab.innerHTML = ''
         while (c <= 10) {
             let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${num*c}`
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
             tab.appendChild(item)
             c++
         }
+    }
+
+
 }
